7fb119f49a87bfd3366d8d6852a5aa492f707ce6
Shubham Prajapat <shubham.prajapat@himanshusofttech.com>
2020-10-04 23:18:55 +0530

    Updates

--- src/components/MockupEditor.js
+++ src/components/MockupEditor.js
@@ -8,9 +8,7 @@ class MockupEditor extends Component {
         super(props);
         let { mockupid } = this.props.match.params;
         this.targetMockup = mockuplist.find((m) => m.id === mockupid);
-        this.state = {
-            redirect: null,
-        };
+        this.apiBase = "http://dev.shubhamprajapat.com/";
 
         this.layerCropInitial = {
             progressText: "Uploading...",
@@ -18,8 +16,8 @@ class MockupEditor extends Component {
             croppedImage: null,
             uploadInProgress: false,
             show: false,
-            image:
-                "http://localhost:3000/static/media/book-mockup.47324234.jpg",
+            layerData: null,
+            image: null,
             crop: { x: 0, y: 0 },
             zoom: 1,
             aspect: 1 / 1,
@@ -27,6 +25,12 @@ class MockupEditor extends Component {
         };
         this.state = {
             layerCropper: this.layerCropInitial,
+            projectid: null,
+            exportedPreview: null,
+            pollPreview: {
+                stage: "INITIAL",
+            },
+            redirect: null,
         };
     }
     dataURItoBlob = (dataURI) => {
@@ -84,6 +88,7 @@ class MockupEditor extends Component {
                     layerCropper: {
                         ...this.state.layerCropper,
                         show: true,
+                        layerData: layer,
                         image: url,
                         aspect,
                     },
@@ -154,12 +159,43 @@ class MockupEditor extends Component {
             </div>
         );
     };
+    pollPreview = () => {
+        this.setState({
+            pollPreview: {
+                ...this.state.pollPreview,
+                stage: "FETCHING",
+            },
+        });
+        fetch(
+            `${this.apiBase}export/${this.targetMockup.id}/${this.state.projectid}`
+        )
+            .then((e) => e.json())
+            .then((res) => {
+                if (!res.url) {
+                    alert("Something went wrong while updating preview.");
+                    return;
+                }
+                this.setState({
+                    exportedPreview: `${this.apiBase}${res.url}`,
+                });
+            })
+            .catch((e) => {
+                alert(`Something went wrong while updating preview : ${e}`);
+            })
+            .finally(() => {
+                this.setState({
+                    pollPreview: {
+                        ...this.state.pollPreview,
+                        stage: "INITIAL",
+                    },
+                });
+            });
+    };
     uploadDisplayPicture = async () => {
         let croppedImage = await getCroppedImg(
             this.state.layerCropper.image,
             this.state.layerCropper.croppedAreaPixels
         );
-        console.log(croppedImage);
         this.setState({
             layerCropper: {
                 ...this.state.layerCropper,
@@ -168,28 +204,6 @@ class MockupEditor extends Component {
                 uploadInProgress: true,
             },
         });
-
-        const setProgress = (p) => {
-            let completeRate = (98 * p) / 100;
-            this.setState({
-                layerCropper: {
-                    ...this.state.layerCropper,
-                    progressText: `Uploading (${completeRate}%)`,
-                },
-            });
-        };
-        let handleUpload = {
-            onStart: () => {
-                setProgress(0);
-            },
-            onProgress: (loaded, total) => {
-                let p = (loaded / total) * 100;
-                setProgress(parseInt(p));
-            },
-            onEnd: () => {
-                setProgress(100);
-            },
-        };
         const _finally = () => {
             this.setState({
                 layerCropper: {
@@ -198,66 +212,107 @@ class MockupEditor extends Component {
                 },
             });
             this.closeCropper();
-            this.props.getUserInfo();
         };
-        let n = "newdp";
         let fd = new FormData();
-        let thumbFileObject = new File([croppedImage], "newdp.jpg");
-        fd.append(n, thumbFileObject, "newdp.jpg");
-        fd.append(
-            "dir",
-            JSON.stringify({
-                newdp: "profile",
-            })
-        );
-
-        /* let uploadReq = postman.upload(
-            "/upload/asset",
-            fd,
-            handleUpload.onStart,
-            handleUpload.onProgress,
-            handleUpload.onEnd
-        );
-        uploadReq.then(async (xhr) => {
-            if (!xhr.responseText.startsWith("{")) {
-                AlertMsg("error", "Network error", "Please try again.");
-                _finally();
-                return;
-            }
-            let res = JSON.parse(xhr.responseText);
-            if (!res.success) {
-                AlertMsg("error", res.message);
-                _finally();
-                return;
-            }
-            if (!res.data["newdp"].uploaded) {
-                AlertMsg("error", res.data["newdp"].error);
-                _finally();
-                return;
-            }
-            let cloudSrc = res.data["newdp"].url;
-            postman
-                .post(
-                    "/usercontent",
-                    {
-                        body: JSON.stringify({
-                            key: "profiles",
-                            data: {
-                                url: cloudSrc,
-                            },
-                        }),
-                    },
-                    { loader: false }
-                )
-                .catch((e) => {
-                    AlertMsg(
-                        "error",
-                        "Something went wrong!",
-                        JSON.stringify(e)
+        let thumbFileObject = new File([croppedImage], "layer.png");
+        fd.append("layer", thumbFileObject, "layer.png");
+        if (this.state.projectid) {
+            fd.append("projectid", this.state.projectid);
+        }
+        let requestOptions = {
+            method: "POST",
+            body: fd,
+            redirect: "follow",
+        };
+        console.log(this.targetMockup.id);
+        fetch(
+            `${this.apiBase}project/${this.targetMockup.id}/${this.state.layerCropper.layerData.id}`,
+            requestOptions
+        )
+            .then((response) => response.json())
+            .then((result) => {
+                if (!result.success) {
+                    alert(
+                        `Something went wrong while uploading selected layer: ${result.message}`
                     );
-                })
-                .finally(_finally);
-        }); */
+                    return;
+                }
+                _finally();
+                this.setState({
+                    projectid: result.projectid,
+                });
+                this.pollPreview();
+            })
+            .catch((error) => {
+                alert(
+                    `Something went wrong while uploading selected layer: ${error}`
+                );
+                _finally();
+                console.log("error", error);
+            });
+    };
+    previewLoader = () => {
+        return (
+            <div className="pr_preview_loader">
+                <svg
+                    width="38"
+                    height="38"
+                    viewBox="0 0 38 38"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <defs>
+                        <linearGradient
+                            x1="8.042%"
+                            y1="0%"
+                            x2="65.682%"
+                            y2="23.865%"
+                            id="a"
+                        >
+                            <stop
+                                stopColor="#fff"
+                                stopOpacity="0"
+                                offset="0%"
+                            />
+                            <stop
+                                stopColor="#fff"
+                                stopOpacity=".631"
+                                offset="63.146%"
+                            />
+                            <stop stopColor="#fff" offset="100%" />
+                        </linearGradient>
+                    </defs>
+                    <g fill="none" fillRule="evenodd">
+                        <g transform="translate(1 1)">
+                            <path
+                                d="M36 18c0-9.94-8.06-18-18-18"
+                                id="Oval-2"
+                                stroke="url(#a)"
+                                strokeWidth="2"
+                            >
+                                <animateTransform
+                                    attributeName="transform"
+                                    type="rotate"
+                                    from="0 18 18"
+                                    to="360 18 18"
+                                    dur="0.9s"
+                                    repeatCount="indefinite"
+                                />
+                            </path>
+                            <circle fill="#fff" cx="36" cy="18" r="1">
+                                <animateTransform
+                                    attributeName="transform"
+                                    type="rotate"
+                                    from="0 18 18"
+                                    to="360 18 18"
+                                    dur="0.9s"
+                                    repeatCount="indefinite"
+                                />
+                            </circle>
+                        </g>
+                    </g>
+                </svg>
+            </div>
+        );
     };
     render() {
         if (this.state.redirect) {
@@ -273,14 +328,48 @@ class MockupEditor extends Component {
         return (
             <Fragment>
                 <div className="pr_container">
+                    <span
+                        className="pr_back"
+                        style={{
+                            cursor: "pointer",
+                        }}
+                        onClick={() => {
+                            this.setState({
+                                redirect: "/",
+                            });
+                        }}
+                    >
+                        Go Back
+                    </span>
                     <h1 className="centered">Edit Mockup</h1>
                     <div className="pr_mockup_editor">
                         <div className="pr_live_mockup_preview">
                             <img
-                                src={this.targetMockup.mockup}
+                                src={
+                                    this.state.exportedPreview ||
+                                    this.targetMockup.mockup
+                                }
                                 alt=""
                                 draggable={false}
                             />
+                            {this.state.pollPreview.stage === "FETCHING" ? (
+                                <this.previewLoader />
+                            ) : null}
+                            {this.state.exportedPreview ? (
+                                <button
+                                    style={{ width: "100%" }}
+                                    onClick={() => {
+                                        let a = document.createElement("a");
+                                        a.href = this.state.exportedPreview;
+                                        a.target = "_blank";
+                                        a.download =
+                                            "psd-replika (shubhamprajapat.com).jpg";
+                                        a.click();
+                                    }}
+                                >
+                                    Download
+                                </button>
+                            ) : null}
                         </div>
                         <div className="pr_layer_edit">
                             <h2 className="m0">Layers</h2>
--- src/mockuplist.js
+++ src/mockuplist.js
@@ -1,12 +1,12 @@
 import bookThumb from "./assets/images/mockups/book-thumb.jpg";
 import tshirtThumb from "./assets/images/mockups/tshirt-thumb.jpg";
 import mugThumb from "./assets/images/mockups/mug-thumb.jpg";
-import packageBox from "./assets/images/mockups/package-box-thumb.jpg";
+import packageBoxThumb from "./assets/images/mockups/package-box-thumb.jpg";
+import packageBox from "./assets/images/mockups/package-box.jpg";
 
 import bookMockup from "./assets/images/mockups/book-mockup.jpg";
-/* import tshirtThumb from "./assets/images/mockups/tshirt-mockcup.jpg";
-import mugThumb from "./assets/images/mockups/mug-mockcup.jpg";
-import packageBox from "./assets/images/mockups/package-box-mockcup.jpg"; */
+import mugMockup from "./assets/images/mockups/mug.jpg";
+import tshirtMock from "./assets/images/mockups/tshirt.jpg";
 let mockuplist = [
     {
         id: "book",
@@ -28,24 +28,52 @@ let mockuplist = [
     },
     {
         id: "package",
-        thumb: packageBox,
+        thumb: packageBoxThumb,
+        mockup: packageBox,
         title: "Package Box",
+        editableLayers: [
+            {
+                id: "sidea",
+                label: "Side A",
+                px: { w: 895, h: 1800 },
+            },
+            {
+                id: "sideb",
+                label: "Side B",
+                px: { w: 895, h: 1800 },
+            },
+        ],
     },
     {
         id: "mug",
         thumb: mugThumb,
+        mockup: mugMockup,
         title: "Mug",
+        editableLayers: [
+            {
+                id: "image",
+                label: "Image",
+                px: { w: 1500, h: 1200 },
+            },
+        ],
     },
     {
         id: "tshirt",
         thumb: tshirtThumb,
+        mockup: tshirtMock,
         title: "Crew Neck Tshirt",
+        editableLayers: [
+            {
+                id: "design",
+                label: "Design",
+                px: { w: 1350, h: 1910 },
+            },
+        ],
     },
 ];
 mockuplist = mockuplist.map((m) => {
     if (m.editableLayers) {
         m.editableLayers = m.editableLayers.map((l) => {
-            console.log(l);
             let ratioX = 1;
             l.ratio = { w: ratioX, h: ratioX / (l.px.w / l.px.h) };
             return l;
@@ -53,5 +81,4 @@ mockuplist = mockuplist.map((m) => {
     }
     return m;
 });
-console.log(mockuplist);
 export default mockuplist;
